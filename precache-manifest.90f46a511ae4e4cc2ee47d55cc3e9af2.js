a3f84d1af18abf679b476e79686da2d1f8e9f395
Ashley Donald <ashd32@gmail.com>
2019-11-19 14:36:04 -0500

    Updates

---
+++ precache-manifest.90f46a511ae4e4cc2ee47d55cc3e9af2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "af00f52d54de9c88b569",
+    "url": "/react-pixel-draw-app/static/css/main.a5bc6ac3.chunk.css"
+  },
+  {
+    "revision": "af00f52d54de9c88b569",
+    "url": "/react-pixel-draw-app/static/js/main.af00f52d.chunk.js"
+  },
+  {
+    "revision": "85ae2e4d129296d3d1ec",
+    "url": "/react-pixel-draw-app/static/js/1.85ae2e4d.chunk.js"
+  },
+  {
+    "revision": "80a8835c3e5f5cdbe499",
+    "url": "/react-pixel-draw-app/static/js/runtime~main.80a8835c.js"
+  },
+  {
+    "revision": "b01307a57b93051324610214ae71a71c",
+    "url": "/react-pixel-draw-app/index.html"
+  }
+];
